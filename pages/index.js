16279c0aca521da67cd609e639bb6887c1299e82
f1reboywatergrl <vincentius.samuelgk2@gmail.com>
2021-12-20 17:29:09 +0700

    Rev: Change gdrive icon link

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ export default function HomePage() {
         <meta name="title" content="I Still Feel as if though I am Me." />
         <meta name="description" content="Masa pandemi COVID-19 yang terjadi di seluruh dunia memaksa setiap individu untuk membatasi kegiatan sosial demi mencegah penyebaran virus COVID-19 yang semakin meluas. Hal tersebut menyebabkan suatu perasaan alienasi yang terjadi disetiap kalangan masyarakat. Manusia tidak merasakan kesehariannya yang biasanya dilakukan secara komunal dan kegiatan bersosialisasi juga terbatas didalam jaringan, menjadi suatu perasaan yang teralienasi. Sudahkan kita menerima diri kita sepenuhnya dan menyadari apa yang telah kita pilih di masa sulit ini? Apakah kita masih merasa menjadi diri kita sendiri?" />
         <meta itemProp="description" content="Masa pandemi COVID-19 yang terjadi di seluruh dunia memaksa setiap individu untuk membatasi kegiatan sosial demi mencegah penyebaran virus COVID-19 yang semakin meluas. Hal tersebut menyebabkan suatu perasaan alienasi yang terjadi disetiap kalangan masyarakat. Manusia tidak merasakan kesehariannya yang biasanya dilakukan secara komunal dan kegiatan bersosialisasi juga terbatas didalam jaringan, menjadi suatu perasaan yang teralienasi. Sudahkan kita menerima diri kita sepenuhnya dan menyadari apa yang telah kita pilih di masa sulit ini? Apakah kita masih merasa menjadi diri kita sendiri?" />
-        <meta itemProp="image" content="https://drive.google.com/uc?id=1IKubR5BqBaRAP8o3df8CckTQQETWYkvb&export=download" />
-        <link rel="icon" href="https://drive.google.com/uc?id=1IKubR5BqBaRAP8o3df8CckTQQETWYkvb&export=download" />
-        <link rel="apple-touch-icon" href="https://drive.google.com/uc?id=1IKubR5BqBaRAP8o3df8CckTQQETWYkvb&export=download" />
+        <meta itemProp="image" content="https://drive.google.com/uc?export=view&id=1kOVo8S8dqB-guY0KVrQjwFHAU0l4lQx8" />
+        <link rel="icon" href="https://drive.google.com/uc?export=view&id=1kOVo8S8dqB-guY0KVrQjwFHAU0l4lQx8" />
+        <link rel="apple-touch-icon" href="https://drive.google.com/uc?export=view&id=1kOVo8S8dqB-guY0KVrQjwFHAU0l4lQx8" />
 
         {/* FB */}
         <meta property="og:type" content="website" />
@@ -20,7 +20,7 @@ export default function HomePage() {
         <meta property="og:title" content="I Still Feel as if though I am Me." />
         <meta property="og:description" content="Masa pandemi COVID-19 yang terjadi di seluruh dunia memaksa setiap individu untuk membatasi kegiatan sosial demi mencegah penyebaran virus COVID-19 yang semakin meluas. Hal tersebut menyebabkan suatu perasaan alienasi yang terjadi disetiap kalangan masyarakat. Manusia tidak merasakan kesehariannya yang biasanya dilakukan secara komunal dan kegiatan bersosialisasi juga terbatas didalam jaringan, menjadi suatu perasaan yang teralienasi. Sudahkan kita menerima diri kita sepenuhnya dan menyadari apa yang telah kita pilih di masa sulit ini? Apakah kita masih merasa menjadi diri kita sendiri?
         " />
-        <meta property="og:image" content="https://drive.google.com/uc?id=1IKubR5BqBaRAP8o3df8CckTQQETWYkvb&export=download" />
+        <meta property="og:image" content="https://drive.google.com/uc?export=view&id=1kOVo8S8dqB-guY0KVrQjwFHAU0l4lQx8" />
         <meta property="og:image:width" content="768" />
         <meta property="og:image:height" content="768" />
 
@@ -30,7 +30,7 @@ export default function HomePage() {
         <meta property="twitter:title" content="I Still Feel as if though I am Me." />
         <meta property="twitter:description" content="Masa pandemi COVID-19 yang terjadi di seluruh dunia memaksa setiap individu untuk membatasi kegiatan sosial demi mencegah penyebaran virus COVID-19 yang semakin meluas. Hal tersebut menyebabkan suatu perasaan alienasi yang terjadi disetiap kalangan masyarakat. Manusia tidak merasakan kesehariannya yang biasanya dilakukan secara komunal dan kegiatan bersosialisasi juga terbatas didalam jaringan, menjadi suatu perasaan yang teralienasi. Sudahkan kita menerima diri kita sepenuhnya dan menyadari apa yang telah kita pilih di masa sulit ini? Apakah kita masih merasa menjadi diri kita sendiri?
         " />
-        <meta property="twitter:image" content="https://drive.google.com/uc?id=1IKubR5BqBaRAP8o3df8CckTQQETWYkvb&export=download" />
+        <meta property="twitter:image" content="https://drive.google.com/uc?export=view&id=1kOVo8S8dqB-guY0KVrQjwFHAU0l4lQx8" />
         <meta property="twitter:image:width" content="768" />
         <meta property="twitter:image:height" content="768" />
       </Head>
